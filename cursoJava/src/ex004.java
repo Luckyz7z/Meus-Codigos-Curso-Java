a52fff6a5c62a449866cbf64e456795c3fc83c65
Lucas Martins <lucasbsb04@gmail.com>
2025-08-19 20:52:51 -0300

    ex

--- cursoJava/src/ex004.java
+++ cursoJava/src/ex004.java
@@ -1,5 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ex004 {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int cod1, cod2, quant1, quant2;
+        double valor1, valor2, valorTotal;
+        System.out.print("Digite o código da peça, quantidade de peças e valor da peça: ");
+        cod1 = sc.nextInt();
+        quant1 = sc.nextInt();
+        valor1 = sc.nextDouble();
+        System.out.print("Digite o código da peça, quantidade de peças e valor da peça: ");
+        cod2 = sc.nextInt();
+        quant2 = sc.nextInt();
+        valor2 = sc.nextDouble();
+        valorTotal = (quant1 * valor1) + (quant2 * valor2);
+        System.out.printf("O valor total é %.2f",valorTotal);
+
     }
 }
